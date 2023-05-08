73d51e163145e37b96b05a9477ab47c5a7f7f4f2
Oladunni Waters <waters.oladunni@gmail.com>
2023-05-08 22:49:53 +0100

    using interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,38 @@
 import './App.css'
+import { useState, useEffect } from 'react';
+
+
+interface Post {
+  idMeal: string; 
+  strMeal: string;
+  strCategory: string;
+}
+
+
 
 function App() {
+  const [posts, setPosts] = useState<Post[]>([]);
 
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+      const data = await response.json();
+      setPosts(data.meals);
+    };
+    fetchPosts();
+  }, []);
 
   return (
-    <>
-        First Typscript project
-    </>
+    <ul>
+      {posts.map((post) => (
+        <li key={post.idMeal}>
+          <h2>{post.strMeal}</h2>
+          <p>{post.strCategory}</p>
+        </li>
+      ))}
+    </ul>
   )
 }
 
 export default App
+
